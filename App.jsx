a96ee3689e8c31d831b979f5a3c9ad988e734804
cedricvoinnet <cedricvoinnet@gmail.com>
2018-12-26 09:38:17 +0100

    Removed configs

--- App.jsx
+++ App.jsx
@@ -2,14 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import firebase from 'firebase';
 
-const config = {
-  apiKey: 'AIzaSyDwQg5UaccjkV4_68R86bDtP8meV2pPK28',
-  authDomain: 'kamgag-9d909.firebaseapp.com',
-  databaseURL: 'https://kamgag-9d909.firebaseio.com',
-  projectId: 'kamgag-9d909',
-  storageBucket: 'kamgag-9d909.appspot.com',
-  messagingSenderId: '428828605117',
-};
+const config = {};
 firebase.initializeApp(config);
 
 const styles = StyleSheet.create({
